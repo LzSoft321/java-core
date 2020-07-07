189dd0a56ea6e0e6a40081176401208dce2ed256
han <xuhan_321@163.com>
2020-07-07 12:25:56 +0800

    阿里OSS文件上传

---
+++ java-core-code/src/main/java/com/lz/javacorecode/advanced/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.lz.javacorecode.advanced;
+
+public class HelloWorld {
+    /* 第一个Java程序
+     * 它将打印字符串 Hello World
+     */
+    public static void main(String []args) {
+		HelloWorld hello = new HelloWorld();
+		 System.out.println(hello); // 打印 Hello World
+		 System.out.println("Hello World"); // 打印 Hello World
+    }
+}
